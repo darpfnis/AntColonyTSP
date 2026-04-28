c7822735eef281f1a18bf7b99c540820bc7db5d3
yuliia <144058436+darpfnis@users.noreply.github.com>
2026-04-29 02:23:57 +0300

    Update Configurations.cs

--- Configurations.cs
+++ Configurations.cs
@@ -1,21 +1,41 @@
 namespace AntColonyTSP;
 
-public sealed class Configurations
+public class Configurations
 {
-    public int Iterations = 100;
-    public int AntCount = 64;
-    public int Goal = 9000;
-    public double StartPheromone = 0.1;
-    public int CityCount = 200;
-    public double DistanceImportance = 2;
-    public double PheromoneImportance = 1;
-    public double CostImportance;
-    public double PathWeightDistance = 1;
-    public double PathWeightCost;
-    public double EvaporationIntensity = 0.1;
-    public int MinDistance = 100;
-    public int MaxDistance = 1000;
-    public int MinCost = 10;
-    public int MaxCost = 500;
-    public int ThreadsCount = 8;
+    public int iterations;
+    public int antCount;
+    public int goalDistance;
+    public int goalCost;
+    public double startPheromone;
+    public int cityCount;
+    public double distanceImportance;
+    public double pheromoneImportance;
+    public double costImportance;
+    public double evaporationIntensity;
+    public int minDistance;
+    public int maxDistance;
+    public int minCost;
+    public int maxCost;
+    public double noPathProbability;
+    public int threadsCount;
+
+    public Configurations()
+    {
+        iterations = 100;
+        antCount = 50;
+        goalDistance = 9000;
+        goalCost = 9000;
+        startPheromone = 0.1;
+        cityCount = 200;
+        distanceImportance = 2;
+        pheromoneImportance = 1;
+        costImportance = 2;
+        evaporationIntensity = 0.1;
+        minDistance = 100;
+        maxDistance = 1000;
+        minCost = 100;
+        maxCost = 1000;
+        noPathProbability = 0.1;
+        threadsCount = 5;
+    }
 }
